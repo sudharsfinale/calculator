38d55fc332964e6fcec4792be96c3b42301f7bf9
sudharsfinale <sudharsfinale@gmail.com>
2025-03-12 23:45:48 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Calculator" }} />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </>
+  );
+}
